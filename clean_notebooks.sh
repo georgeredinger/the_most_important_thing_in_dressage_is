6f16e625214681fb6f1f1273bca6c3699326e72d
George Redinger <george@georgeredinger.com>
2012-04-09 15:03:17 -0700

    cleaned up clean_notebooks.sh

--- clean_notebooks.sh
+++ clean_notebooks.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-for f in *.html; do
-  sed '1,/class="author-name"/d' "$f"  \
-		| sed '/class="grid_3 omega"/,$d' \
+for f in notebook_*.html; do
+  sed '1,/class="content clear-block"/d' "$f"  \
+		| sed '/name="read-and-post"/,$d' \
+		| sed '/<script type="text\/javascript"/,/<\/script>/d' \
 		| sed 's/<[^>]*>//g' \
-		| sed 's/\&nbsp;//g' \
-		| sed '/^$/d'| sed 's/^[ \t]*//' \
-		| sed '/›/d' \
-		| sed '/»/d' \
 		| fmt -w 1 \
-    | sed  's/^[a-zA-Z0-9]{1,3}$//'
-done
+		| sed 's/\&.*\;//' \
+		| sed 's/^[ »›\r\n]*//' \
+	  | sed '/[\,\.\+\-\!]/d' \
+		| sed '/^[a-zA-Z][a-zA-Z]$/d' \
+		| sed '/^[a-zA-Z]$/d' \
+		| sed '/^M/d' \
+		| sed '/^\n\n$/d' \
+		| sed '/^\n$/d' \
+		| sed 's/^[ \t]*//;s/[ \t]*$//' \
+    | sed '/.\{3\}/!d'
+done 
+
+#for f in *.html; do
+#  sed '1,/class="author-name"/d' "$f"  \
+#		| sed '/class="grid_3 omega"/,$d' \
+#		| sed 's/<[^>]*>//g' \
+#		| sed 's/\&nbsp;//g' \
+#		| sed '/^$/d'| sed 's/^[ \t]*//' \
+#		| sed '/›/d' \
+#		| sed '/»/d' \
+#		| fmt -w 1 \
+#    | sed  's/^[a-zA-Z0-9]{1,3}$//'
+#done
 
