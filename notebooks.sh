a806f0e00d2da7ea336e77e9374a659bc9ba70c3
George Redinger <george@georgeredinger.com>
2012-04-09 11:17:26 -0700

    redundant notebooks.txt

--- notebooks.sh
+++ notebooks.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo > notebooks.txt
 for i  in {1..1000} ; do 
 	notebook_number=`printf "%04d" "$i"`
 	curl -s "http://barnbynotes.com/open-notebooks/$i"  > "notebook_$notebook_number.html"
