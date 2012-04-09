ddc2168320c3d8d47ef1ee2778a09dce642b298c
George Redinger <george@georgeredinger.com>
2012-04-09 11:20:32 -0700

    guess there are 2000 notebooks

--- notebooks.sh
+++ notebooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i  in {1..1000} ; do 
+for i  in {1..2000} ; do 
 	notebook_number=`printf "%04d" "$i"`
 	curl -s "http://barnbynotes.com/open-notebooks/$i"  > "notebook_$notebook_number.html"
 done
